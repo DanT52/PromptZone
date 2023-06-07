2bd17a46c6c85accd05c840ff0a994eda704d547
DanT52 <dantramp52@gmail.com>
2023-06-07 00:32:52 -0700

    login +save use to db

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -42,7 +42,7 @@ const Nav = () => {
                         Sign Out
                     </button>
                     <Link href="/profile">
-                        <Image src="/assets/images/logo.svg"
+                        <Image src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
@@ -71,7 +71,7 @@ const Nav = () => {
         <div className='sm:hidden flex relative'>
             {session?.user ? (
                 <div className='flex'>
-                    <Image src="/assets/images/logo.svg"
+                    <Image src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
