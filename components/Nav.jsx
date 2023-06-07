3e5724bd54479b62e8e4b64a5d155ecf2e78be6c
DanT52 <dantramp52@gmail.com>
2023-06-06 20:44:02 -0700

    session navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,18 +5,18 @@ import Image from 'next/image'
 import {useState, useEffect} from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 const Nav = () => {
-    const isUserLoggedIn = true
+    const {data: session} = useSession()
     const [providers, setProviders ] = useState(null);
 
     const [toggleDropdown, setToggleDropDown] = useState(false)
 
     useEffect(() => {
-        const setProviders = async () =>{
+        const setUpProviders = async () =>{
             const response = await getProviders()
 
             setProviders(response)
         }
-        setProviders()
+        setUpProviders()
     }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -32,7 +32,7 @@ const Nav = () => {
         {/* Desktop navigation*/}
 
         <div className='sm:flex hidden'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link href="/create-prompt" className='black_btn'>
                         Create Post
@@ -69,7 +69,7 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className='sm:hidden flex relative'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex'>
                     <Image src="/assets/images/logo.svg"
                         width={37}
