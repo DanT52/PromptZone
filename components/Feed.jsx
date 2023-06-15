5fada98c143f8dc7cd41d2939231de1d1ad78849
DanT52 <dantramp52@gmail.com>
2023-06-15 01:07:34 -0700

    feed and crompt card update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-import PromptCard from "./PromptCard"
+import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
+  
   return (
     <div className="mt-16 prompt_layout">
+      
       {data.map((post) =>(
         <PromptCard
           key={post._id}
@@ -21,23 +23,23 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('')
-  const [posts, setPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([]);
+  
 
   const handleSearchChange = (e) => {
 
   }
-  
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
   useEffect(() => {
-    const fetchPosts = async () =>{
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-
-      setPosts(data)
-    }
-
-    fetchPosts()
-
-  }, [])
+    fetchPosts();
+  }, []);
 
   return (
     <section className="feed">
@@ -51,7 +53,7 @@ const Feed = () => {
               />
       </form>
       <PromptCardList
-        data={[posts]}
+        data={allPosts}
         handleTagClick={() =>{}}
 
       />
