450a2d2a1f53630adfa56c0180f25ba4aaaa3ee4
DanT52 <dantramp52@gmail.com>
2023-06-16 16:46:04 -0700

    profile stuff

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,14 @@ import { usePathname, useRouter } from 'next/navigation'
 
 
 const PromptCard = ( { post, handleTagClick, handleEdit, handleDelete}) => {
+  const [copied, setCopied] = useState("")
+
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(()=> setCopied(""), 3000)
+  }
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
@@ -20,12 +28,33 @@ const PromptCard = ( { post, handleTagClick, handleEdit, handleDelete}) => {
             className="rounded-full object-contain"
             />
             <div className='flex flex-col'>
-              <h3>{post.creator.username}</h3>
-              <p>{post.creator.email}</p>
-
+              <h3 className='font-satoshi font-semibold text-grey-900'>
+                {post.creator.username}
+              </h3>
+              <p className='font-inter text-sm text-gray-500'>
+                {post.creator.email}
+              </p>
             </div>
         </div>
+
+        <div className="cpy_btn" onClick={() => handleCopy()}>
+          <Image
+            src={copied === post.prompt 
+            ? '/assets/icons/tick.svg'
+            : '/assets/icons/copy.svg'}
+            width={12}
+            height={12}
+          />
+
+        </div>
       </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick
+        (post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   )
 }
